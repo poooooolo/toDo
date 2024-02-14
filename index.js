128392501c9184da51c8219cfbcfcc8d17fce2a3
polo <paupolope@gmail.com>
2024-02-14 14:47:24 +0100

    configuración de entorno

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+require("dotenv").config();
 const express = require("express");
 
 const servidor = express();
 
-servidor.listen(3000);
+servidor.listen(process.env.PORT);
+//las variables de entorno sivern para guardar datos sensibles como la BBDD contraseñas etc
